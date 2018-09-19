96db80a048ea02283081c06a9747a26eaed121b6
Manuel Quesada Segura <manuelquesadasegura@gmail.com>
2018-09-19 07:42:52 +0200

    lifecycle components

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import StarRating from './StarRating'
+import '../styles/color.css'
 
-const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
+export default class Color extends Component {
+  componentWillMount() {
+    this.style = { backgroundColor: "#CCC" }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { rating } = this.props
+    return rating !== nextProps.rating
+  }
+
+  componentWillUpdate(nextProps) {
+    const { title, rating } = this.props
+    this.style = null
+    this.refs.title.style.backgroundColor = "red"
+    this.refs.title.style.color = "white"
+    alert(`${title}: rating ${rating} -> ${nextProps.rating}`)
+  }
+
+  componentDidUpdate(prevProps) {
+    const { title, rating } = this.props
+    const status = (rating > prevProps.rating) ? 'better' : 'worse'
+    this.refs.title.style.backgroundColor = ""
+    this.refs.title.style.color = "black"
+  }
+
+  render() {
+    const { title, rating, color, onRate } = this.props
+    return (
+      <section className="color-section" style={this.style}>
+        <h1 ref="title">{title}</h1>
+        <div  className="color"
+              style={{ backgroundColor: color }}
+        >
+        </div>
+        <StarRating starsSelected={rating}
+                    onRate={onRate}
+        />
+      </section>
+    )
+  }
+}
+
+Color.propTypes = {
+  title: PropTypes.string,
+  rating: PropTypes.number,
+  color: PropTypes.string,
+  onRate: PropTypes.func
+}
+
+Color.defaultProps = {
+  title: undefined,
+  rating: 0,
+  color: "#000000",
+  onRate: f=>f
+}
+
+/*const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
   <section className="color">
     <h1>{title}</h1>
     <button onClick={onRemove}>X</button>
@@ -15,4 +73,4 @@ const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
     </div>
   </section>
 
-  export default Color
+  export default Color*/
